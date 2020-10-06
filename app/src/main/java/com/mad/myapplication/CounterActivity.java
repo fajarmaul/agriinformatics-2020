c5ced73aec6cb2690a1d02f838a0ee853c31f670
Fajar Maulana <fajar.maulana@digiasia.asia>
2020-10-06 23:00:04 +0700

    Menambahkan Komentar

--- app/src/main/java/com/mad/myapplication/CounterActivity.java
+++ app/src/main/java/com/mad/myapplication/CounterActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 public class CounterActivity extends AppCompatActivity {
 
+    //Global Variable, variable ini bisa diakses di seluruh fungsi yang terdapat di dalam kelas (CounterActivity) ini
     Button tombolMinus, tombolPlus;
     TextView counterTV;
 
@@ -19,6 +20,9 @@ public class CounterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
 
+        //Variabel lokal, hanya bisa diakses di dalam fungsi ini (onCreate)
+        String nama = "John";
+
         tombolMinus = findViewById(R.id.minusButton);
         tombolPlus = findViewById(R.id.plusButton);
         counterTV = findViewById(R.id.counterTV);
@@ -27,7 +31,7 @@ public class CounterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 nilai = nilai + 1;
-                counterTV.setText(String.valueOf(nilai));
+                setNilaiCounter();
             }
         });
 
@@ -35,8 +39,14 @@ public class CounterActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 nilai = nilai - 1;
-                counterTV.setText(String.valueOf(nilai));
+                setNilaiCounter();
             }
         });
+    }
+
+    // Perhatikan bagaimana counterTV bisa diakses di fungsi ini.
+    // Cobalah memanggil variabel 'nama' yang ada di fungsi onCreate ke dalam fungsi ini, apakah variabel tersebut bisa diakses ?
+    private void setNilaiCounter(){
+        counterTV.setText(String.valueOf(nilai));
     }
 }
--- app/src/main/java/com/mad/myapplication/MainActivity.java
+++ app/src/main/java/com/mad/myapplication/MainActivity.java
@@ -8,27 +8,43 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+//Sebuah kelas, atau sederhananya untuk saat ini adalah tempat kita meletakkan kode dari program yang mau kita jalankan
 public class MainActivity extends AppCompatActivity {
 
+    //Baris ini adalah baris komentar, tidak akan diperhatikan oleh kode ketika di 'compile'
+    //Biasanya diawali dengan dua buah garis miring
+
+
+    //Awal mula program di android di jalankan
+    //Untuk saat ini, anggap fungsi ini irip seperti fungsi "main" ketika kalian mencoba pemrograman C atau python
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Ini adalah 'binding', mengikat komponen UI yang ada di XML, dengan variabel yang ada di file java ini
         Button tombolMulai = findViewById(R.id.mulaiButton);
+
+        //Memasang "Pendengar Klik" pada tombolmulai. 'Pendengar' atau listener ini maksudnya, ketika tombol di klik, dia mau menjalankan apa
+        //Harus diperhatikan, tombol harus sudah di binding terlebih dahulu, atau nanti akan muncul 'NullPointerException'
         tombolMulai.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //Uncomment line 34 dengan menekan CTRL dan ? di saat yang bersamaan untuk membuat program menjalankan perintah di line 34
+                //tampilkanToast();
+
                 pindahHalaman();
             }
         });
 
     }
 
+    //Sebuah fungsi yang tujuannya adalah menampilkan 'Toast'
     private void tampilkanToast(){
         Toast.makeText(this, "Tombol ini di klik", Toast.LENGTH_SHORT).show();
     }
 
+    //Fungsi untuk pindah halaman
     private void pindahHalaman(){
 
         //Buat intent untuk pindah halaman di sini
